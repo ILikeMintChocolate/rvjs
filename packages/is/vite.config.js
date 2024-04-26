9f1fc7dcb5e2b3078af746d4db9b91670431b868
곽범석 <qjatjr99@naver.com>
2024-04-27 01:21:57 +0900

    is-chore: create type check library

---
+++ packages/is/vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    minify: 'terser',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@rvjs/is',
+      fileName: (format) => `index.${format}.js`,
+    },
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    exclude: ['@rvjs/core'],
+  },
+  sourcemap: true,
+  emptyOutDir: true,
+})
