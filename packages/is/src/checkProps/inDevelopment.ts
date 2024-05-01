b7a59630796cdfc19a8277e2e9996d4102a73a68
ILikeMintChocolate <qjatjr99@naver.com>
2024-05-01 23:16:53 +0900

    is-feature: add checkProps function to validate props

---
+++ packages/is/src/checkProps/inDevelopment.ts
@@ -0,0 +1,44 @@
+import {
+  createInvalidTypeErrorMessage,
+  createNoValidatorErrorMessage,
+  createUndefinedPropErrorMessage,
+} from './error.ts'
+
+const checkPropsInDevelopment = <Props>(
+  props: Props,
+  types: Record<keyof Props, Function>,
+) => {
+  const uncheckedProps = { ...types }
+
+  // @ts-ignore
+  Object.keys(props).forEach((key) => {
+    // @ts-ignore
+    const value = props[key]
+    // @ts-ignore
+    const validator = types[key]
+
+    if (!validator) {
+      const errorMessage = createNoValidatorErrorMessage(key)
+      console.error(errorMessage)
+    } else if (!validator(value)) {
+      const errorMessage = createInvalidTypeErrorMessage(
+        key,
+        value,
+        validator.name,
+      )
+      console.error(errorMessage)
+    }
+
+    // @ts-ignore
+    delete uncheckedProps[key]
+  })
+
+  Object.keys(uncheckedProps).forEach((key) => {
+    const errorMessage = createUndefinedPropErrorMessage(key)
+    console.error(errorMessage)
+  })
+
+  return props
+}
+
+export default checkPropsInDevelopment
--- packages/is/src/index.ts
+++ packages/is/src/index.ts
@@ -1,3 +1,4 @@
 export * from './type/primitive'
 export * from './type/reference'
 export * from './type/rvjs'
+export { default as checkProps } from './checkProps'
