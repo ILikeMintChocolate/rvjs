13a90a7e60a74700c2a97b3d31fbe2315b5ba860
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-02 04:23:37 +0900

    ui-feature: implement section components

---
+++ packages/ui/src/component/layout/section/Section.css.ts
@@ -0,0 +1,7 @@
+import vars from '@theme/variable/vars.css.ts'
+import { style } from '@vanilla-extract/css'
+
+export const section_style = style({
+  marginTop: vars.spacing['05'],
+  marginBottom: vars.spacing['05'],
+})
