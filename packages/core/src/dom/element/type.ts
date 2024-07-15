7f00f81688f0eae83ff9b10686c90a26edb0dd7b
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-16 03:14:04 +0900

    core-fix: fix element types

---
+++ packages/core/src/dom/element/type.ts
@@ -0,0 +1,41 @@
+import { CustomProps } from '@element/property.ts'
+import { Dynamic } from '@hook/dynamic.ts'
+import { Properties } from 'csstype'
+
+type AllHTMLProps = {
+  [Tag in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[Tag]
+}
+
+export type AllElementProps = CustomProps & {
+  [K in keyof FilteredAllElementProps]:
+    | FilteredAllElementProps[K]
+    | Dynamic<FilteredAllElementProps[K]>
+}
+
+type FilteredAllElementProps = {
+  [Tag in keyof AllHTMLProps]: FilterProps<AllHTMLProps[Tag]>
+}[keyof AllHTMLProps]
+
+export type ElementProps<TagName extends keyof HTMLElementTagNameMap> =
+  CustomProps & {
+    [K in keyof FilteredElementProps<TagName>]:
+      | FilteredElementProps<TagName>[K]
+      | Dynamic<FilteredElementProps<TagName>[K]>
+  }
+
+type FilteredElementProps<TagName extends keyof HTMLElementTagNameMap> =
+  FilterProps<HTMLElementTagNameMap[TagName]>
+
+export type StyleProps = {
+  [K in keyof Properties]: Properties[K] | Dynamic<Properties[K]>
+}
+
+export type SvgProps = CustomProps & {
+  [K in keyof FilteredSvgElementProps]:
+    | FilteredSvgElementProps[K]
+    | Dynamic<FilteredSvgElementProps[K]>
+}
+
+type FilteredSvgElementProps = FilterProps<SVGElement>
+
+type FilterProps<Props> = Omit<Props, 'style' | 'children' | 'className'>
