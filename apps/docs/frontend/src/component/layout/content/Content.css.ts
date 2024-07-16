7fcb7dd98ce3495552617d12881df41926540ee5
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-17 04:05:48 +0900

    docs-fe-feature: implement Content component

---
+++ apps/docs/frontend/src/component/layout/content/Content.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const content_wrapper_style = style({
+  flexDirection: 'row',
+  justifyContent: 'center',
+})
+
+export const content_inner_style = style({
+  width: '48rem',
+})
