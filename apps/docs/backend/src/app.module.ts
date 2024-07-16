757be87cdae2548370fd5fc798aec9718a94723c
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-17 02:47:13 +0900

    docs-be-feature: implement functions to find JSON content

--- apps/docs/backend/src/app.module.ts
+++ apps/docs/backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ContentController } from './content/content.controller'
+import { ContentService } from './content/content.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ContentController],
+  providers: [ContentService],
 })
 export class AppModule {}
