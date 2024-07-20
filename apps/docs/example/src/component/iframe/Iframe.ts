0e093ca3aba7cff04bd899c90bf2698ebccad9d3
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-21 02:08:47 +0900

    docs-ex-feature: implement Iframe component

---
+++ apps/docs/example/src/component/iframe/Iframe.ts
@@ -0,0 +1,25 @@
+import {
+  iframe_style,
+  iframe_wrapper_style,
+} from '@component/iframe/Iframe.css.ts'
+import { Child, div } from '@rvjs/core/dom'
+
+interface IframeProps {
+  content: Child
+}
+
+const Iframe = (props: IframeProps) => {
+  const { content } = props
+
+  return div({
+    classes: [iframe_wrapper_style],
+    children: [
+      div({
+        classes: [iframe_style],
+        children: [content],
+      }),
+    ],
+  })
+}
+
+export default Iframe
