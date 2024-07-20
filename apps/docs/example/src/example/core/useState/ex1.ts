89d3a5a90f7fe2bf170bbbe5664527521d3e48fb
ILikeMintChocolate <qjatjr99@naver.com>
2024-07-21 02:11:05 +0900

    docs-ex-feature: implement useState-1 example

---
+++ apps/docs/example/src/example/core/useState/ex1.ts
@@ -0,0 +1,39 @@
+import {
+  button_style,
+  buttonWrapper_style,
+  heading_style,
+  wrapper_style,
+} from '@example/core/useState/ex1.css.ts'
+import { button, div, h1 } from '@rvjs/core/dom'
+import { dynamic, useState } from '@rvjs/core/reactive'
+
+const Counter = () => {
+  const [getCount, setCount] = useState(0)
+
+  return div({
+    classes: [wrapper_style],
+    children: [
+      h1({
+        classes: [heading_style],
+        textContent: dynamic(() => `Count: ${getCount()}`),
+      }),
+      div({
+        classes: [buttonWrapper_style],
+        children: [
+          button({
+            textContent: '-1',
+            classes: [button_style],
+            onclick: () => setCount(getCount() - 1),
+          }),
+          button({
+            textContent: '+1',
+            classes: [button_style],
+            onclick: () => setCount(getCount() + 1),
+          }),
+        ],
+      }),
+    ],
+  })
+}
+
+export default Counter
