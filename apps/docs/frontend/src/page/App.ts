d8b1ec3f8e6ccd29f79486907b14bbe413b7b636
ILikeMintChocolate <qjatjr99@naver.com>
2024-08-14 03:06:04 +0900

    docs-fe-feature: implement Router

--- apps/docs/frontend/src/page/App.ts
+++ apps/docs/frontend/src/page/App.ts
@@ -1,82 +1,78 @@
-import Content from '@layout/content/Content.ts'
-import Header from '@layout/header/Header.ts'
-import SideNav from '@layout/sideNav/SideNav.ts'
-import { component, div, Suspense } from '@rvjs/core/dom'
-import { Router, useOutlet, usePathParams } from '@rvjs/core/router'
-import { Flex } from '@rvjs/ui/layout'
-import { App as ShellApp, Body } from '@rvjs/ui/shell'
-import NoContentError from '../component/error/NoContentError.ts'
-import { useGetContent } from '../hook/useContent.ts'
+import NoContentError from '@component/error/NoContentError.ts'
+import Header from '@component/header/Header.ts'
+import SideNav from '@component/sideNav/SideNav.ts'
+import ContentData from '@page/Content.ts'
+import {
+  component,
+  div,
+  onMount,
+  Router,
+  Suspense,
+  useNavigate,
+  useOutlet,
+  usePathname,
+} from '@rvjs/core'
+import { Shell } from '@rvjs/ui'
 
 const App = () => {
-  return ShellApp({
+  return Shell({
     header: Header(),
     panel: SideNav(),
-    body: Body({
-      children: [
-        Router({
-          '/:pId': {
-            componentFn: () => MainBody(),
+    body: Router({
+      '/:pId': {
+        componentFn: () => MainBody(),
+        router: {
+          '/getting-started': {
+            componentFn: () => ContentPage(),
+          },
+          '/:cId': {
+            componentFn: () => CategoryPage(),
             router: {
-              '/getting-started': {
+              '/:fId': {
                 componentFn: () => ContentPage(),
-              },
-              '/:cId': {
-                componentFn: () => CategoryPage(),
-                router: {
-                  '/:fId': {
-                    componentFn: () => ContentPage(),
-                  },
-                },
               },
             },
           },
-        }),
-      ],
+        },
+      },
     }),
   })
 }
 
 const MainBody = component(() => {
   const outlet = useOutlet()
+  const navigate = useNavigate()
+  const pathname = usePathname()
+
+  onMount(() => {
+    if (pathname() === '/') {
+      navigate('/core/getting-started')
+    }
+  })
 
   return div({
+    style: {
+      flex: '1',
+    },
     children: [outlet],
   })
 })
 
 const CategoryPage = component(() => {
   const outlet = useOutlet()
-
   return div({
+    style: {
+      width: '100%',
+    },
     children: [outlet],
   })
 })
 
 const ContentPage = component(() => {
-  return Content({
-    children: [
-      Suspense({
-        content: () => ContentData(),
-        error: NoContentError(),
-      }),
-    ],
+  return Suspense({
+    content: () => ContentData(),
+    error: NoContentError(),
   })
 })
-
-const ContentData = async () => {
-  const { pId, cId = 'getting-started', fId } = usePathParams()!
-  const requestPath = [pId, cId, fId].filter(Boolean).join('/')
-  const content = await useGetContent(requestPath)
-
-  return Flex({
-    direction: 'column',
-    style: {
-      paddingTop: '1rem',
-      paddingBottom: '4rem',
-    },
-    children: content,
-  })
-}
 
 export default App
