c2d29778121b6e88e80f77dc0e455b80b00b5043
ILikeMintChocolate <qjatjr99@naver.com>
2024-08-14 02:59:49 +0900

    docs-fe-chore: modify alias settings

--- apps/docs/frontend/vite.config.js
+++ apps/docs/frontend/vite.config.js
@@ -8,20 +8,32 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@layout',
-        replacement: resolve(__dirname, 'src/component/layout'),
+        find: '@component',
+        replacement: resolve(__dirname, 'src/component'),
+      },
+      {
+        find: '@hook',
+        replacement: resolve(__dirname, 'src/hook'),
+      },
+      {
+        find: '@page',
+        replacement: resolve(__dirname, 'src/page'),
+      },
+      {
+        find: '@style',
+        replacement: resolve(__dirname, 'src/style'),
       },
       {
         find: '@util',
         replacement: resolve(__dirname, 'src/util'),
       },
       {
-        find: '@icon',
-        replacement: resolve(__dirname, 'src/asset/icon'),
-      },
-      {
         find: '@asset',
         replacement: resolve(__dirname, 'src/asset'),
+      },
+      {
+        find: '@icon',
+        replacement: resolve(__dirname, 'src/asset/icon'),
       },
     ],
   },
