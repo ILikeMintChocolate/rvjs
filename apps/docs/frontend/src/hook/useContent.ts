114836894893fec3526c354a3c1f715cc34c116d
ILikeMintChocolate <qjatjr99@naver.com>
2024-08-14 03:06:25 +0900

    docs-fe-feature: implement Content page

--- apps/docs/frontend/src/hook/useContent.ts
+++ apps/docs/frontend/src/hook/useContent.ts
@@ -1,15 +1,31 @@
-import { renderComponentFromJSON } from '@rvjs/ui/util'
+import { renderComponentFromJSON } from '@rvjs/ui'
 import { getApiPath } from '@util/api.ts'
+
+const contentCache = {}
 
 export const useGetContent = async (path: string) => {
   const apiPath = getApiPath(path)
+  if (contentCache[apiPath]) {
+    return contentCache[apiPath]
+  }
   const rawContent = await fetchContent(apiPath)
-  const renderedContent = renderComponentFromJSON(rawContent)
-  return renderedContent
+  const { blocks: renderedContent, context } = renderComponentFromJSON(
+    rawContent,
+    {
+      indexHeading: true,
+    },
+  )
+  contentCache[apiPath] = {
+    content: renderedContent,
+    headingIndex: context.headingIndex,
+  }
+  return { content: renderedContent, headingIndex: context.headingIndex }
 }
 
 export const fetchContent = async (path: string) => {
-  const response = await fetch(path, { cache: 'force-cache' })
+  const response = await fetch(path, {
+    cache: 'force-cache',
+  })
   if (!response.ok) {
     throw new Error(`Failed to fetch content: ${response.statusText}`)
   }
