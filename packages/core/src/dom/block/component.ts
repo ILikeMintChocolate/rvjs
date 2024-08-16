dd53736c0e883a166fd41c37132fad808d1f542e
ILikeMintChocolate <qjatjr99@naver.com>
2024-08-16 13:48:10 +0900

    core-fix: implement lazy render component

--- packages/core/src/dom/block/component.ts
+++ packages/core/src/dom/block/component.ts
@@ -1,15 +1,28 @@
 import { Block } from '@block/block.ts'
 import { LifecycleHandlers } from '@block/util/lifecycleHandlers.ts'
 import { RouteContext } from '@block/util/routeContext.ts'
-import { HTMLNode } from '@element/type.ts'
-import { isElement, isTextNode } from '@type/rvjs.ts'
+import { componentContext } from '@context/executionContext.ts'
+import { isComponent, isElement, isTextNode } from '@type/rvjs.ts'
 
 export class ComponentBlock extends RouteContext(LifecycleHandlers(Block)) {
   #key: string | null
+  #lazyRenderContext: {
+    tempElement: Comment
+    isRendered: boolean
+    renderFn: Function
+    previousComponent: Block
+  }
 
   constructor() {
     super({ type: 'COMPONENT' })
     this.#key = null
+    this.#lazyRenderContext = {
+      tempElement: null,
+      isRendered: false,
+      renderFn: null,
+      previousComponent: null,
+    }
+    this.#initialRender()
   }
 
   set key(value: string | null) {
@@ -20,19 +33,50 @@ export class ComponentBlock extends RouteContext(LifecycleHandlers(Block)) {
     return this.#key
   }
 
-  appendChild(child: Block) {
+  set renderFn(renderFn: Function) {
+    this.#lazyRenderContext.renderFn = renderFn
+  }
+
+  get tempElement() {
+    return this.#lazyRenderContext.tempElement
+  }
+
+  #initialRender() {
+    const comment = document.createComment('lazy-component')
+    this.#lazyRenderContext.tempElement = comment
+    this.nodes = [comment]
+  }
+
+  triggerLazyRender() {
+    componentContext.set(this)
+    const renderedChild = this.#lazyRenderContext.renderFn()
+    this.#lazyRenderContext.renderFn = null
+    this.#lazyRenderContext.isRendered = true
+    if (isComponent(this.#lazyRenderContext.previousComponent)) {
+      componentContext.set(this.#lazyRenderContext.previousComponent)
+    }
+    this.#swapNodesForLazyRender(renderedChild)
+  }
+
+  isRendered() {
+    return this.#lazyRenderContext.isRendered
+  }
+
+  #swapNodesForLazyRender(child: Block) {
     this.child = child
     this.domLength = child.domLength
     child.parent = this
-    const newNodes: HTMLNode[] = []
     const rerenderableContexts = []
-    if (isElement(child) || isTextNode(child)) {
-      newNodes.push(child.element)
-    } else {
-      newNodes.push(...child.nodes)
+    if (!(isElement(child) || isTextNode(child))) {
       rerenderableContexts.push({ block: child, localDOMIndex: 0 })
     }
-    this.nodes = newNodes
     this.rerenderableContexts = rerenderableContexts
+    const fragment = document.createDocumentFragment()
+    if (isElement(child) || isTextNode(child)) {
+      fragment.append(child.element)
+    } else {
+      fragment.append(...child.nodes)
+    }
+    this.tempElement.replaceWith(fragment)
   }
 }
