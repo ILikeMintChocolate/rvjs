d1dfe4873c4abee61df86678885c5f0055cddde2
ILikeMintChocolate <qjatjr99@naver.com>
2024-08-30 01:57:44 +0900

    docs-ex-fix: modify to intercept console.error

--- apps/docs/example/src/util/console.ts
+++ apps/docs/example/src/util/console.ts
@@ -2,11 +2,17 @@ import { useGlobalState } from '@rvjs/core'
 
 export const overrideConsoleLog = () => {
   const originalConsoleLog = console.log
+  const originalConsoleError = console.error
   const [logs, setLogs] = useGlobalState<string[]>('LOGS', [])
 
   console.log = (...args) => {
     originalConsoleLog.apply(console, args)
     addLogMessage(args)
+  }
+
+  console.error = (...args) => {
+    originalConsoleError.apply(console, args)
+    setLogs([...logs(), args[0]])
   }
 
   const addLogMessage = (logArgs: any[]) => {
