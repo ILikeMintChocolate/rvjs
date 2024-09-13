9c6c46ba8c504f1bdf81b88dda9255e578fad948
ILikeMintChocolate <qjatjr99@naver.com>
2024-09-14 04:22:04 +0900

    docs-fe-feature: add loading Spinner

---
+++ apps/docs/frontend/src/component/loading/Loading.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const loading_wrapper_style = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+  marginTop: '-10%',
+})
