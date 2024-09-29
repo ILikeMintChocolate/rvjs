c564c6a8c1ad8ac0d7ac110ea238c26b8e41a592
ILikeMintChocolate <qjatjr99@naver.com>
2024-09-30 07:37:20 +0900

    ui-fix: fix some styles

--- packages/ui/src/component/content/list/orderedList/OrderedList.css.ts
+++ packages/ui/src/component/content/list/orderedList/OrderedList.css.ts
@@ -1,5 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
 export const orderedList_style = style({
+  display: 'flex',
+  flexDirection: 'column',
   margin: 0,
 })
--- packages/ui/src/component/content/list/unorderedList/UnorderedList.css.ts
+++ packages/ui/src/component/content/list/unorderedList/UnorderedList.css.ts
@@ -1,7 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
 export const unorderedList_style = style({
-  margin: 0,
   display: 'flex',
   flexDirection: 'column',
+  margin: 0,
 })
