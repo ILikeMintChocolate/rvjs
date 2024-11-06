191137baa7aaa1507b4bffdefae3c983b06504a2
ILikeMintChocolate <qjatjr99@naver.com>
2024-11-07 05:41:13 +0900

    core-v0.3-fix: fix duplicate item in For component

--- packages/core/src/block/renderer/forRenderer.ts
+++ packages/core/src/block/renderer/forRenderer.ts
@@ -12,7 +12,8 @@ export const ForRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
     each: unknown[]
     startNode: Comment
     endNode: Comment
-    itemMap: Map<unknown, Component>
+    itemMap: Map<unknown, { items: Component[]; index: number }>
+    countMap: Map<unknown, number>
     deletable: Component[]
     committable: Component[]
 
@@ -22,6 +23,7 @@ export const ForRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
       this.self.startNode = document.createComment('FOR-COMPONENT-START')
       this.self.endNode = document.createComment('FOR-COMPONENT-END')
       this.itemMap = new Map()
+      this.countMap = new Map()
       this.deletable = []
       this.committable = []
     }
@@ -49,23 +51,39 @@ export const ForRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
     }
 
     renderItems(renderFn: (item: unknown) => (Component | Node)[]) {
-      const newItemMap = new Map()
+      const newItemMap = new Map<
+        unknown,
+        { items: Component[]; index: number }
+      >()
+      const newCountMap = new Map<unknown, number>()
       const children = []
-      this.self.each.forEach((item) => {
+      for (let i = 0; i < this.self.each.length; i++) {
+        const item = this.self.each[i]
+        let itemData = newItemMap.get(item)
+        if (!itemData) {
+          itemData = { items: [], index: 0 }
+          newItemMap.set(item, itemData)
+        }
         let child
-        if (this.self.itemMap.has(item)) {
-          child = this.self.itemMap.get(item)
+        if ((this.self.countMap.get(item) ?? 0) !== 0) {
+          const existingItem = this.self.itemMap.get(item)
+          child = existingItem.items[existingItem.index++]
+          this.self.countMap.set(item, (this.self.countMap.get(item) ?? 1) - 1)
         } else {
           child = new BlockComponent(() => renderFn(item), 'BLOCK_COMPONENT')
           this.self.setParentChildRelation(child)
           this.committable.push(child)
         }
-        this.self.itemMap.delete(item)
         children.push(child)
-        newItemMap.set(item, child)
-      })
-      this.self.deletable = [...this.self.itemMap.values()]
+        itemData.items.push(child)
+        newCountMap.set(item, (newCountMap.get(item) ?? 0) + 1)
+      }
+      const itemMapValues = [...this.self.itemMap.values()]
+      this.self.deletable = itemMapValues.flatMap(({ items, index }) =>
+        items.slice(index),
+      )
       this.self.itemMap = newItemMap
+      this.self.countMap = newCountMap
       return children
     }
 
