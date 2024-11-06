f255105345a2a5d9f5f30e1310b0746f1e60891a
ILikeMintChocolate <qjatjr99@naver.com>
2024-11-07 06:41:59 +0900

    core-v0.3-chore: enable code minification on build

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       circleDependency({
         outputFilePath: './circleDep',
       }),
-      mode === 'development' && vitePluginRvjs(),
+      mode !== 'production' && vitePluginRvjs(),
     ],
     build: {
       lib: {
