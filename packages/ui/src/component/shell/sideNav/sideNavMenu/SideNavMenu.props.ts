9ab0115e2fe44863eaf593caaa56097323f9e6a4
ILikeMintChocolate <qjatjr99@naver.com>
2024-12-13 23:10:54 +0900

    ui-jsx-fix: update SideNavMenu component

--- packages/ui/src/component/shell/sideNav/sideNavMenu/SideNavMenu.props.ts
+++ packages/ui/src/component/shell/sideNav/sideNavMenu/SideNavMenu.props.ts
@@ -1,45 +1,24 @@
-import { Children, prop, Prop, RefObject } from '@rvjs/core'
-import {
-  isBoolean,
-  isChildren,
-  isFunction,
-  isNumber,
-  isObjectType,
-  isOptional,
-  isProp,
-  isString,
-} from '@rvjs/is'
+import { Component, ElementObject } from '@rvjs/core'
 import { EventHandlers } from '@type/event.ts'
 
 export interface SideNavMenuProps {
-  menuName: Prop<string>
-  children: Children
-  ariaLabel?: Prop<string>
-  focusRef?: RefObject<HTMLButtonElement>
+  menuName: string
+  children: (Component | Node)[]
+  ariaLabel?: string
+  focusElement?: ElementObject<HTMLButtonElement>
   onBlur?: EventHandlers['onBlur']
   onClick?: EventHandlers['onClick']
-  tabIndex?: Prop<number>
-  defaultShow?: Prop<boolean>
-}
-
-export const sideNavMenuPropsType = {
-  menuName: isProp(isString),
-  children: isChildren,
-  ariaLabel: isOptional(isProp(isString)),
-  focusRef: isOptional(isObjectType),
-  onBlur: isOptional(isFunction),
-  onClick: isOptional(isFunction),
-  tabIndex: isOptional(isProp(isNumber)),
-  defaultShow: isOptional(isProp(isBoolean)),
+  tabIndex?: number
+  defaultShow?: boolean
 }
 
 export const sideNavMenuRenderProps = {
-  menuName: (p: string) => prop(() => p),
-  children: (p: Children) => p,
-  ariaLabel: (p: string) => prop(() => p),
-  focusRef: (p: Object) => p,
-  onBlur: (p: Function) => p,
-  onClick: (p: Function) => p,
-  tabIndex: (p: number) => prop(() => p),
-  defaultShow: (p: boolean) => prop(() => p),
+  menuName: (p: SideNavMenuProps['menuName']) => p,
+  children: (p: SideNavMenuProps['children']) => p,
+  ariaLabel: (p: SideNavMenuProps['ariaLabel']) => p,
+  focusElement: (p: SideNavMenuProps['focusElement']) => p,
+  onBlur: (p: SideNavMenuProps['onBlur']) => p,
+  onClick: (p: SideNavMenuProps['onClick']) => p,
+  tabIndex: (p: SideNavMenuProps['tabIndex']) => p,
+  defaultShow: (p: SideNavMenuProps['defaultShow']) => p,
 }
