2df92445fdd6310b41c35047090cfebb3e8169c1
ILikeMintChocolate <qjatjr99@naver.com>
2024-12-18 03:50:44 +0900

    ui-jsx-fix: fix Text component props

--- packages/ui/src/component/typography/text/Text.props.ts
+++ packages/ui/src/component/typography/text/Text.props.ts
@@ -4,6 +4,7 @@ import { TextStyleProps } from '@typography/text/Text.css.ts'
 export interface TextProps extends TextStyleProps {
   as?: keyof HTMLElementTagNameMap
   className?: string
+  text?: string
   children?: Children
 }
 
@@ -12,5 +13,6 @@ export const textRenderProps = {
   kind: (p: TextProps['kind']) => p,
   color: (p: TextProps['color']) => p,
   className: (p: TextProps['className']) => p,
+  text: (p: TextProps['text']) => p,
   children: (p: TextProps['children']) => p,
 }
