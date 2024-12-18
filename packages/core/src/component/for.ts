fb753b3448e3acb64d3be5b8c9ea92576dbbf2d8
ILikeMintChocolate <qjatjr99@naver.com>
2024-12-18 16:36:13 +0900

    core-jsx-fix: fix For component to use index options

--- packages/core/src/component/for.ts
+++ packages/core/src/component/for.ts
@@ -1,12 +1,13 @@
-import { Component } from '@block/component/component.ts'
 import { ForComponent } from '@block/component/for.ts'
 import { componentContext } from '@context/component.ts'
+import { GetState } from '@hook/useState.ts'
+import { Children } from '@type/jsx.ts'
 import { copyGetter } from '@util/function.ts'
 import { RVJS_COMPONENT_FN_IDENTIFIER } from '@util/identifier.ts'
 
 interface ForProps<T> {
   each: T[]
-  children: (item: T) => (Component | Node)[]
+  children: (item: T, index?: GetState<number>) => Children
 }
 
 export const For = <T>(props: ForProps<T>) => {
