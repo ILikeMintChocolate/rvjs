b06c8ff52751ee25c79bcc16f4990e12058e7149
ILikeMintChocolate <qjatjr99@naver.com>
2024-12-19 23:00:49 +0900

    core-jsx-fix: fix Switch renderer

--- packages/core/src/block/renderer/caseRenderer.ts
+++ packages/core/src/block/renderer/caseRenderer.ts
@@ -1,7 +1,6 @@
 import { BlockComponent } from '@block/component/block.ts'
 import { CaseComponent } from '@block/component/case.ts'
 import { Component } from '@block/component/component.ts'
-import { SwitchComponent } from '@block/component/switch.ts'
 import { Constructor, Empty } from '@block/util/mixin.ts'
 import { componentContext } from '@context/component.ts'
 import { stateContext } from '@context/state.ts'
@@ -19,8 +18,8 @@ export const CaseRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
     constructor(...args: any[]) {
       super(...args)
       this.self = this as unknown as CaseComponent
-      this.self.startNode = null
-      this.self.endNode = null
+      this.self.startNode = document.createComment('CASE-COMPONENT-START')
+      this.self.endNode = document.createComment('CASE-COMPONENT-END')
       this.self.isMounted = false
     }
 
@@ -38,12 +37,14 @@ export const CaseRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
         this.self.deleteItem()
         return
       }
-      const child = (this.self.renderFn() as Component[])[0]
-      this.self.startNode = document.createComment('CASE-COMPONENT-START')
-      this.self.endNode = document.createComment('CASE-COMPONENT-END')
+      const children = this.self.renderFn() as Component[]
       this.self.isMounted = true
-      ;(this.self.parent as SwitchComponent).updateSwitchDom(
-        convertToNodes([child]),
+      this.self.updateDom(
+        this.self.parentNode ?? this.self.startNode.parentNode,
+        this.self.startNode,
+        this.self.endNode,
+        convertToNodes(children),
+        this.self.startNode.nextSibling === this.self.endNode,
       )
       if (!isInitial) {
         this.self.commitItem()
@@ -63,8 +64,8 @@ export const CaseRenderer = <TBase extends Constructor<Empty>>(Base: TBase) => {
 
     deleteItem() {
       this.self.isMounted = false
+      this.self.clearDom(this.self.startNode, this.self.endNode)
       this.self.childComponents.forEach((child) => {
-        child.destroyNode()
         child.destroyComponent()
       })
       this.self.childComponents = []
