ea35b693bb19c5968c1e8cfa15c81421ee14ee47
ILikeMintChocolate <qjatjr99@naver.com>
2024-12-22 05:12:30 +0900

    ui-jsx-fix: update to use Defined component

--- packages/ui/src/component/shell/header/headerName/HeaderName.tsx
+++ packages/ui/src/component/shell/header/headerName/HeaderName.tsx
@@ -1,7 +1,7 @@
+import { Defined } from '@rvjs/core'
 import { headerName_style } from '@shell/header/headerName/HeaderName.css.ts'
 import { useHeaderNameNavigation } from '@shell/header/headerName/HeaderName.hook.ts'
 import { HeaderNameProps } from '@shell/header/headerName/HeaderName.props.ts'
-import { isDefined } from '@type/guard.ts'
 import Text from '@typography/text/Text.tsx'
 
 const HeaderName = (props: HeaderNameProps) => {
@@ -9,11 +9,11 @@ const HeaderName = (props: HeaderNameProps) => {
 
   return (
     <a className={headerName_style} href={props.href} onClick={onClickHandler}>
-      {isDefined(props.prefix) && (
+      <Defined value={props.prefix}>
         <Text kind="body-compact-01" color="textPrimary">
           {props.prefix}
         </Text>
-      )}
+      </Defined>
       <Text kind="heading-compact-01" color="textPrimary">
         {props.title}
       </Text>
