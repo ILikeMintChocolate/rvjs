35a95136655d70b0c361eeebab5a9c15fe6d4b0f
ILikeMintChocolate <qjatjr99@naver.com>
2025-02-15 03:37:19 +0900

    docs-be-refactor: remove directory

--- apps/docs/backend/src/error/noContent.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common'
-
-export class NoContentException extends HttpException {
-  constructor() {
-    super('Data not found', HttpStatus.INTERNAL_SERVER_ERROR)
-  }
-}
